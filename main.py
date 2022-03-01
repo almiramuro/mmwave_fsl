2a03e5f5025d6a786922c7c08e09f0b58fcb264c
almiramuro <45156777+almiramuro@users.noreply.github.com>
2022-03-02 00:01:55 +0800

    Classified preprocessing codes under the pre-processing folder; added points visualizations through text file

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-﻿print('Hello, World!')
+﻿if __name__=="__main__":
+    
+    cpic = open("car.txt", "r")
+    for x in cpic:
+        print(x)
---
+++ pre-processing/tryplot.py
@@ -0,0 +1,23 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+def print3D(points):
+    xs = points[:,0]
+    ys = points[:,1]
+    zs = points[:,2]
+
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.set_box_aspect((np.ptp(xs), np.ptp(ys), np.ptp(zs)))
+    
+          
+    ax.plot(xs, ys, zs)
+    plt.show()
+
+
+if __name__=="__main__":
+    ptlist=np.loadtxt("car.txt")
+
+    print(ptlist)
+    print3D(ptlist)
