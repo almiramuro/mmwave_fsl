452c361fd6f791174342bc8e95b8f6fd661dc3c5
almiramuro <45156777+almiramuro@users.noreply.github.com>
2022-04-30 15:33:15 +0800

    added points pre-processing

---
+++ pre-processing/readlog.py
@@ -0,0 +1,19 @@
+﻿import json
+import numpy as np
+
+def make_it_dict(string):
+    s = string.replace("'",'"')
+    return json.loads(s)
+
+if __name__ == "__main__":
+    
+    instances = []
+    
+    with open("pymmw_data.log",'r') as data:
+        entries = data.readlines()
+    
+    for line in entries:
+        instances.append(make_it_dict(line))
+        break
+    print(instances[0]["dataFrame"]["detected_points"])
+    # print(make_it_dict(instances[0]["dataFrame"])["detected_points"])
--- pre-processing/tryplot.py
+++ pre-processing/tryplot.py
@@ -1,23 +1,72 @@
 ﻿import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from sklearn.cluster import DBSCAN
+import json
+import pickle
 
-def print3D(points):
-    xs = points[:,0]
-    ys = points[:,1]
-    zs = points[:,2]
+def cluster_wcolor(extractedPts, e = 0.1, min_samp = 3):
+    """
+        extractedPts: type = ndarray (n x 3)
+        with colors --> for realtime nalang
+    """
 
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    ax.set_box_aspect((np.ptp(xs), np.ptp(ys), np.ptp(zs)))
+    db = DBSCAN(eps=e, min_samples=min_samp).fit(extractedPts)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+
+    labels = db.labels_
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
     
-          
-    ax.scatter(xs, ys, zs)
-    plt.show()
+    xyzc = dict()
 
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = (0, 0, 0, 1)
+
+        class_member_mask = labels == k
+
+        xyz = extractedPts[class_member_mask & core_samples_mask]
+        
+        xyz = np.vstack((xyz,extractedPts[class_member_mask & ~core_samples_mask]))
+        xyzc[col] = xyz
+    
+    # for col, pt in xyzc.items(): 
+    #     print(col,':\n',pt,'\n---------------')
+
+def cluster(extractedPts, e = 0.1, min_samp = 3):
+    """
+        extractedPts: type = ndarray (n x 3)
+        without colors
+    """
+    
+    db = DBSCAN(eps=e, min_samples=min_samp).fit(extractedPts)
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+
+    labels = db.labels_
+    unique_labels = set(labels)
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    
+    xyzc = dict()
+
+    for k in unique_labels:
+        class_member_mask = labels == k
+
+        xyz = extractedPts[class_member_mask & core_samples_mask]
+        
+        xyz = np.vstack((xyz,extractedPts[class_member_mask & ~core_samples_mask]))
+        xyzc[k] = xyz
+
+    for k, pt in xyzc.items(): 
+        print(k,':\n',pt,'\n---------------')
 
 if __name__=="__main__":
-    ptlist=np.loadtxt("car.txt")
 
-    print(ptlist)
-    print3D(ptlist)
+    with open('raw_2022-04-30_15-04-31.pkl',"rb") as pm_data:
+        pm_contents = pickle.load(pm_data,encoding ="bytes")
+    
+    for ts, pts in pm_contents.items():
+        print('time: ',ts)
+        cluster(pts, e = 0.1)
