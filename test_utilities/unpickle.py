8e8a705592cdba06da3d64b426b7e07e8957b9e8
almiramuro <45156777+almiramuro@users.noreply.github.com>
2022-06-24 09:32:41 +0800

    minor revisions

--- test_utilities/unpickle.py
+++ test_utilities/unpickle.py
@@ -5,29 +5,29 @@ from math import log10
 import matplotlib.animation
 
 
-filename = 'aaron_batangas_1'
-with open(filename+'.pkl', 'rb') as handle:
+filename = '..\\feellazy\\indoor\\alma_reel_lazy_10.pkl'
+with open(filename, 'rb') as handle:
     data = pickle.load(handle)
-
+print(filename)
 print(data)
 
-t = list(data.keys())
+# t = list(data.keys())
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-title = ax.set_title('3D Test')
-ax.set_xlim3d(-3, 3)
-ax.set_ylim3d(-3, 3)
-ax.set_zlim3d(-3, 3)
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# title = ax.set_title('3D Test')
+# ax.set_xlim3d(-3, 3)
+# ax.set_ylim3d(-3, 3)
+# ax.set_zlim3d(-3, 3)
 
-points = data[t[0]]
+# points = data[t[0]]
 
-xs = points[:,0]
-ys = points[:,1]
-zs = points[:,2]
+# xs = points[:,0]
+# ys = points[:,1]
+# zs = points[:,2]
 
-graph = ax.scatter(xs, ys, zs)
+# graph = ax.scatter(xs, ys, zs)
 
-# ani = matplotlib.animation.FuncAnimation(fig, update, t, interval=33, blit=False)
+# # ani = matplotlib.animation.FuncAnimation(fig, update, t, interval=33, blit=False)
 
-plt.show()
+# plt.show()
